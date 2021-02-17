3bb7712068c40a0fd5c10252e17fb626932f4080
Maorshl <56290456+Maorshl@users.noreply.github.com>
2021-02-17 17:44:03 +0200

    Added GET all
    gets a list of all objects in Bins.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,25 @@
-const { request, response } = require("express");
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const uuid = require("uuid");
 
 app.use(express.json());
 
 app.post("/b", (req, res) => {
   const { body } = req;
-  const id = Math.random().toString(36).substr(2, 9);
-  fs.writeFileSync(`bin - ${id}.json`, JSON.stringify(body, null, 4));
+  const id = uuid.v4().padStart(6, "0");
+  fs.writeFileSync(`./Bins/bin - ${id}.json`, JSON.stringify(body, null, 4));
   res.status(200).send(body);
+});
+
+app.get("/b", (req, res) => {
+  const requestedBin = fs.readdirSync(`./Bins`);
+  res.status(200).send(requestedBin);
 });
 
 app.get("/b/:id", (req, res) => {
   const requestedBinId = req.params.id;
-  const requestedBin = fs.readFileSync(`./bin - ${requestedBinId}.json`);
+  const requestedBin = fs.readFileSync(`./Bins/bin - ${requestedBinId}.json`);
   res.status(200).send(JSON.parse(requestedBin));
 });
 
@@ -22,16 +27,16 @@ app.put("/b/:id", (req, res) => {
   const { body } = req;
   const requestedBinId = req.params.id;
   fs.writeFileSync(
-    `bin - ${requestedBinId}.json`,
+    `./Bins/bin - ${requestedBinId}.json`,
     JSON.stringify(body, null, 4)
   );
-  const newJson = fs.readFileSync(`./bin - ${requestedBinId}.json`);
+  const newJson = fs.readFileSync(`./Bins/bin - ${requestedBinId}.json`);
   res.status(200).send(JSON.parse(newJson));
 });
 
 app.delete("/b/:id", (req, res) => {
   const requestedBinId = req.params.id;
-  fs.unlinkSync(`./bin - ${requestedBinId}.json`, (error) => {
+  fs.unlinkSync(`./Bins/bin - ${requestedBinId}.json`, (error) => {
     if (error) {
       console.error(error);
     }
